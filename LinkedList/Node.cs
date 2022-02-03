ba9e9005edeca8e2127f7c1904a9e0d2ef8874b1
vishal <vishalpalode9899@gmail.com>
2022-02-04 00:28:25 +0530

    Ordered linkedlist in ascending order

--- LinkedList/Node.cs
+++ LinkedList/Node.cs
@@ -18,4 +18,18 @@ namespace LinkedList
             this.next = null;
         }
     }
+    public class SortedNode<T>
+    {
+        public T data;
+        public SortedNode<T> next;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SortedNode{T}"/> class.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        public SortedNode(T data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
 }
